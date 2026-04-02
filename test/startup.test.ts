29adb23453ad46b39d728b65e785ba737b9f32a2
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2026-04-02 19:23:03 +0800

    fix: correct test tsconfig for LSP diagnostics

--- test/startup.test.ts
+++ test/startup.test.ts
@@ -3,6 +3,7 @@ import { config } from "../package.json";
 
 describe("startup", function () {
   it("should have plugin instance defined", function () {
-    assert.isNotEmpty(Zotero[config.addonInstance]);
+    const plugin = (Zotero as Record<string, unknown>)[config.addonInstance];
+    assert.isNotEmpty(plugin);
   });
 });
